8db59d9a1280e9a41f83c00ff5273b1c8188c287
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-04-18 15:04:00 +0100

    suggestion from review

--- src/category_theory/connected.lean
+++ src/category_theory/connected.lean
@@ -220,42 +220,4 @@ lemma nat_trans_from_connected [conn : connected J] {X Y : C}
   (λ j, α.app j)
   (λ _ _ f, (by { have := α.naturality f, erw [id_comp, comp_id] at this, exact this.symm }))
 
-omit 𝒞 𝒥
-section examples
-instance cospan_inhabited : inhabited walking_cospan := ⟨walking_cospan.one⟩
-
-instance cospan_connected : connected (walking_cospan) :=
-begin
-  apply connected.of_induct,
-  introv _ t,
-  cases j,
-  { rwa t walking_cospan.hom.inl },
-  { rwa t walking_cospan.hom.inr },
-  { assumption }
-end
-
-instance span_inhabited : inhabited walking_span := ⟨walking_span.zero⟩
-
-instance span_connected : connected (walking_span) :=
-begin
-  apply connected.of_induct,
-  introv _ t,
-  cases j,
-  { assumption },
-  { rwa ← t walking_span.hom.fst },
-  { rwa ← t walking_span.hom.snd },
-end
-
-instance parallel_pair_inhabited : inhabited walking_parallel_pair := ⟨walking_parallel_pair.one⟩
-
-instance parallel_pair_connected : connected (walking_parallel_pair) :=
-begin
-  apply connected.of_induct,
-  introv _ t, cases j,
-  { rwa t walking_parallel_pair_hom.left },
-  { assumption }
-end
-
-end examples
-
 end category_theory
